af4809fcb006c99aa611f0a301d1a88e130c437b
Nolan Duarte <nolball777@gmail.com>
2025-05-04 13:04:39 -0400

    updated navbar and added blog article

---
+++ script/nav.js
@@ -0,0 +1,36 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("hidden");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (event.target.matches('.dropbtn') /* && !(event.target.nodeName =='P')*/ ) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+
+
+}
+
+window.onclick = function(event) {
+    if (event.target.matches('.dropbtn') /* && !(event.target.nodeName =='P')*/ ) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('hidden')) {
+          openDropdown.classList.remove('hidden');
+        }
+      }
+    }
+  
+  
+  }
+
